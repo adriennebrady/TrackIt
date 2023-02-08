9e6e84ddea7bf8f813597d32441d5c2aba03bed8
Israel Herrera <israel.herrera@ufl.edu>
2023-02-07 19:31:27 -0500

    Updated testing to reflect pointers

--- Inv/platform/inventory/inv_test.go
+++ Inv/platform/inventory/inv_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	inv := New()
-	inv.Add(InvItem{"check", "dfj"})
+	inv.Add(&InvItem{"check", "dfj"})
 
 	if len(inv.InvItems) != 1 {
 		t.Errorf("Item was not added")
@@ -14,7 +14,7 @@ func TestAdd(t *testing.T) {
 func TestGetAll(t *testing.T) {
 
 	inv := New()
-	inv.Add(InvItem{})
+	inv.Add(&InvItem{})
 	results := inv.GetAll()
 	if len(results) != 1 {
 		t.Errorf("Item was not added")
@@ -23,25 +23,19 @@ func TestGetAll(t *testing.T) {
 
 func TestRename(t *testing.T) {
 	inv := New()
-	inv.Add(InvItem{"ac", "de"})
-	inv.Rename("ac", "newdfsdf")
+	inv.Add(&InvItem{"ac", "de"})
+	inv.Rename(inv.InvItems["ac"], "newdfsdf")
 
 	if inv.InvItems["newdfsdf"].Name != "newdfsdf" {
 		t.Errorf("Item was not renamed")
 	}
 
-	if inv.InvItems["newdfsdf"].Name == "newdfsdf" {
-		t.Errorf("Item was renamed")
-	}
-	
-
 }
-
 
 func TestRelocate(t *testing.T) {
 	inv := New()
-	inv.Add(InvItem{"ac", "de"})
-	inv.Relocate("ac", "newdfsdf")
+	inv.Add(&InvItem{"ac", "de"})
+	inv.Relocate(inv.InvItems["ac"], "newdfsdf")
 
 	if inv.InvItems["ac"].Location != "newdfsdf" {
 		t.Errorf("Item was not relocated")
