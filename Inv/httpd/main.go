707f55f92f386100a177e90141b3b063724c5f25
Israel Herrera <israel.herrera@ufl.edu>
2023-02-14 19:35:43 -0500

    Adding persistent database

--- Inv/httpd/main.go
+++ Inv/httpd/main.go
@@ -4,11 +4,38 @@ import (
 	"Trackit/Inv/httpd/handler"
 	"Trackit/Inv/platform/inventory"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string
+}
+
 func main() {
 	inv := inventory.New()
+
+	db, err := gorm.Open(sqlite.Open("AllTracks.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Alice", Email: "alice@example.com"})
+
+	var user User
+	db.First(&user, 1)
+
+	db.Model(&user).Update("Name", "Bob")
+
+	//db.Delete(&user, 1)
+
+	// Create a new user
 	//fmt.Println(inv)
 	//inv.Add(1, 2, 3)
 	//fmt.Println(inv)
