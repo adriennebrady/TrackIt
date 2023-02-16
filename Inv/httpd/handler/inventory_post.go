528a5c2dc5e7438331ad263b970b87648372b002
TAnDe07 <tdesir1@ufl.edu>
2023-02-15 21:28:55 -0500

    update to containers

--- Inv/httpd/handler/inventory_post.go
+++ Inv/httpd/handler/inventory_post.go
@@ -8,6 +8,7 @@ import (
 )
 
 type InvRequest struct {
+	Kind 	 string `json:"Kind"` // container or item?
 	Name     string `json:"Name"`
 	Location string `json:"Location"`
 	Type     string `json:"Type"`
@@ -18,19 +19,39 @@ func InventoryPost(inv inventory.Poster) gin.HandlerFunc {
 		requestBody := InvRequest{}
 		c.Bind(&requestBody)
 
-		invItem := inventory.InvItem{
-			Name:     requestBody.Name,
-			Location: requestBody.Location,
+		if  requestBody.Kind == "Container" {
+			container := inventory.Container{
+				Name:     requestBody.Name,
+				Location: requestBody.Location,
+			}
+				
+			if requestBody.Type == "Add" {
+				inv.AddContainer(&container)
+			}
+			if requestBody.Type == "Rename" {
+				inv.RenameContainer(container.Name, container.Location)
+			}
+			if requestBody.Type == "Relocate" {
+				inv.RelocateContainer(container.Name, container.Location)
+			}
+
+		} else {
+			invItem := inventory.InvItem{
+				Name:     requestBody.Name,
+				Location: requestBody.Location,
+			}
+			
+			if requestBody.Type == "Add" {
+				inv.Add(&invItem)
+			}
+			if requestBody.Type == "Rename" {
+				inv.Rename(invItem.Name, invItem.Location)
+			}
+			if requestBody.Type == "Relocate" {
+				inv.Relocate(invItem.Name, invItem.Location)
+			}
 		}
-		if requestBody.Type == "Add" {
-			inv.Add(&invItem)
-		}
-		if requestBody.Type == "Rename" {
-			inv.Rename(invItem.Name, invItem.Location)
-		}
-		if requestBody.Type == "Relocate" {
-			inv.Relocate(invItem.Name, invItem.Location)
-		}
+
 		c.Status(http.StatusNoContent)
 
 	}
