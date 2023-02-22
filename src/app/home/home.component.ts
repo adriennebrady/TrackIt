6440c738016bb96b6765618cc5287bc08b1a423a
evaeia <adrienne@cyberbrady.com>
2023-02-21 20:32:20 -0500

    Navbar now reflects if user is logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private authService: AuthService) {}
 
+  loggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  logOut() {
+    this.authService.logout();
+  }
 }
