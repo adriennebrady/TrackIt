fbe2cb2f450bebda51f2d8746fb3543e580e7482
Israel Herrera <israel.herrera@ufl.edu>
2023-03-13 09:54:24 -0400

    Updates TODOS

--- Inv/httpd/handler/ping_get.go
+++ Inv/httpd/handler/ping_get.go
@@ -14,14 +14,9 @@ func PingGet() gin.HandlerFunc {
 	}
 }
 
-//TODO create a container when someone registers
-//TODO get that rootLocID from the register or login post request along with the token?
-
-//TODO Delete CONTAINERS
-
-//TODO trash for recently deleted
-
+//TODO Delete CONTAINERS (And accounts)
 //TODO Multi user inventories
 //TODO import/export inventories
-//TODO possibly later more complex recursive version of get container name
-//TODO allow users to delete accounts (which requires deleting their root container)
+//TODO More complex recursive version of get container name
+
+//TODO trash for recently deleted
