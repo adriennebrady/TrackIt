bd660025a43b208ed4274d6b577d40b916b685c6
Sara <sara.winner@ufl.edu>
2023-03-28 17:57:38 -0400

    Inv container unit test should create failure fix

--- src/app/inventory-page/inv-container/inv-container.component.spec.ts
+++ src/app/inventory-page/inv-container/inv-container.component.spec.ts
@@ -2,7 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InvContainerComponent } from './inv-container.component';
 import { InventoryPageComponent } from '../../inventory-page/inventory-page.component';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 
 describe('InvContainerComponent', () => {
   let component: InvContainerComponent;
@@ -12,13 +19,19 @@ describe('InvContainerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        InvContainerComponent,
-        MatDialogModule
+        InvContainerComponent        
+      ],
+      imports: [
+        HttpClientTestingModule,
+        HttpClientModule,
+        MatDialogModule,
+        MatCardModule,
+        MatIconModule,
+        MatMenuModule,
+        MatDividerModule
       ],
       providers: [
-        InventoryPageComponent,
-        { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MatDialogRef, useValue: {} }
+        InventoryPageComponent
       ]
     })
     .compileComponents();
