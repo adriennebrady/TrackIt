5e5a6b71f9eb40a62957ac2571595aaaee75f706
Israel Herrera <israel.herrera@ufl.edu>
2023-03-30 20:36:19 -0400

    Update todos

--- Inv/httpd/handler/ping_get.go
+++ Inv/httpd/handler/ping_get.go
@@ -14,8 +14,12 @@ func PingGet() gin.HandlerFunc {
 	}
 }
 
-//TODO Multi user inventories
-//TODO import/export inventories
-//TODO More complex recursive version of get container name
-
-//TODO trash for recently deleted
+//TODO: Multi user inventories
+//TODO: import/export inventories
+//TODO: More complex recursive version of get container name
+//TODO: recently deleted page
+//TODO: separate inventory get for sidebar
+//TODO: account deletion page
+//TODO: ITEM count/ container description
+//TODO: possible cards
+//TODO: add item tags
