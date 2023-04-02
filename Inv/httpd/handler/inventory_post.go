482abd2bd45d2d90c0673989248c78e90ff2c5f6
Israel Herrera <israel.herrera@ufl.edu>
2023-04-02 17:14:57 -0400

    optimize post

--- Inv/httpd/handler/inventory_post.go
+++ Inv/httpd/handler/inventory_post.go
@@ -14,7 +14,7 @@ type InvRequest struct {
 	Cont          int    `json:"Cont"`
 	Name          string `json:"Name"`
 	Type          string `json:"Type"`
-	Count		  int    `json:"Count"` //for items
+	Count         int    `json:"Count"` //for items
 }
 
 func InventoryPost(db *gorm.DB) gin.HandlerFunc {
@@ -31,38 +31,43 @@ func InventoryPost(db *gorm.DB) gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 			return
 		}
+		switch requestBody.Kind {
+		case "container":
+			{
+				newContainer := Container{
+					LocID:    requestBody.ID,
+					Name:     requestBody.Name,
+					ParentID: requestBody.Cont,
+					User:     username,
+				}
 
-		if requestBody.Kind == "container" {
-			newContainer := Container{
-				LocID:    requestBody.ID,
-				Name:     requestBody.Name,
-				ParentID: requestBody.Cont,
-				User:     username,
+				if result := db.Table("containers").Create(&newContainer); result.Error != nil {
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create container"})
+					return
+				}
 			}
+		case "item":
+			{
+				newItem := Item{
+					ItemID:   requestBody.ID,
+					User:     username,
+					ItemName: requestBody.Name,
+					LocID:    requestBody.Cont,
+					Count:    requestBody.Count,
+				}
 
-			if result := db.Table("containers").Create(&newContainer); result.Error != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create container"})
+				if result := db.Table("items").Create(&newItem); result.Error != nil {
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create item"})
+					return
+				}
+			}
+		default:
+			{
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
 				return
 			}
-		} else if requestBody.Kind == "item" {
-			newItem := Item{
-				ItemID:   requestBody.ID,
-				User:     username,
-				ItemName: requestBody.Name,
-				LocID:    requestBody.Cont,
-				Count:    requestBody.Count,
-			}
-
-			if result := db.Table("items").Create(&newItem); result.Error != nil {
-				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create item"})
-				return
-			}
-		} else {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
-			return
 		}
 
 		c.Status(http.StatusNoContent)
-
 	}
 }
