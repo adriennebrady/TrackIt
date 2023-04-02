32829aa41fc1aadd795c5c81a7e35372d58aff5a
Israel Herrera <israel.herrera@ufl.edu>
2023-04-02 18:34:12 -0400

    Possible treeget

---
+++ Inv/httpd/handler/tree_get.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ContainerTree struct {
+	Container Container
+	Children  []ContainerTree
+}
+
+func TreeGet(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		// Verify that the token is valid.
+		var username string
+		if username = IsValidToken(token, db); username == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		// Get the root location of the user.
+		var existingUser Account
+		if result := db.Table("accounts").Where("username = ?", username).First(&existingUser); result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "User does not exist"})
+			return
+		}
+
+		// Recursively add children containers with their path to the result.
+		containerTree := ContainerTree{
+			Container: Container{LocID: existingUser.RootLoc},
+			Children:  getChildren(existingUser.RootLoc, "", db),
+		}
+
+		c.JSON(http.StatusOK, containerTree)
+	}
+}
+
+func getChildren(parentID int, parentPath string, db *gorm.DB) []ContainerTree {
+	var containers []Container
+	if result := db.Table("Containers").Where("parentID = ?", parentID).Find(&containers); result.Error != nil {
+		return nil
+	}
+
+	containerTree := make([]ContainerTree, 0)
+	for _, container := range containers {
+		childPath := parentPath + "/" + container.Name
+		childTree := ContainerTree{
+			Container: container,
+			Children:  nil, // initialize to nil in case there are no children
+		}
+		children := getChildren(container.LocID, childPath, db)
+		if children != nil {
+			childTree.Children = children
+		}
+		containerTree = append(containerTree, childTree)
+	}
+
+	return containerTree
+}
