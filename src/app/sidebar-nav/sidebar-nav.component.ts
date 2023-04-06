c192dd25e210b4fbfe9aa3096b01b97cf21618fb
evaeia <adrienne@cyberbrady.com>
2023-04-06 13:32:41 -0400

    Added NEW sidebar navigation tree w/buttons to directly go to container

---
+++ src/app/sidebar-nav/sidebar-nav.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+interface Container {
+  LocID: number;
+  Name: string;
+  ParentID: number;
+  User: string;
+}
+
+interface ContainerTree {
+  Container: Container;
+  Children: ContainerTree[];
+}
+
+/** Flat node with expandable and level information */
+interface ContainerFlatNode {
+  expandable: boolean;
+  Container: Container;
+  level: number;
+}
+
+@Component({
+  selector: 'app-sidebar-nav',
+  templateUrl: './sidebar-nav.component.html',
+  styleUrls: ['./sidebar-nav.component.css'],
+})
+export class SidebarNavComponent {
+  private _transformer = (node: ContainerTree, level: number) => {
+    return {
+      expandable: node.Children && node.Children.length > 0,
+      Container: node.Container,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<ContainerFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.Children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor(private http: HttpClient) {
+    this.getContainerTree().subscribe((data) => {
+      this.dataSource.data = data.Children;
+    });
+  }
+
+  hasChild = (_: number, node: ContainerFlatNode) => node.expandable;
+
+  getContainerTree(): Observable<ContainerTree> {
+    // Set the HTTP headers with the authorization token
+    const authToken: string = localStorage.getItem('token')!;
+
+    const authorization = {
+      Authorization: authToken,
+    };
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: authorization.Authorization,
+      }),
+    };
+    return this.http.get<ContainerTree>('/api/tree', httpOptions);
+  }
+}
