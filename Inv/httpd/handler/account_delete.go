11926bcecf6c1f215ec674864e8af9804e5f3c7a
Israel Solano <israel.herrera@ufl.edu>
2023-04-12 08:48:37 -0400

    Compartmentalize function

--- Inv/httpd/handler/account_delete.go
+++ Inv/httpd/handler/account_delete.go
@@ -38,16 +38,7 @@ func AccountDelete(DB *gorm.DB) gin.HandlerFunc {
 		// Start a new transaction to ensure atomicity.
 		tx := DB.Begin()
 
-		var maxLocID int64
-		mpty := false
-		var cont Container
-		if result := DB.Table("items").Where("container_id = ?", cont.LocID).Count(&maxLocID); result.Error != nil {
-			// Handle error
-			mpty = true
-			maxLocID = 0;
-		}
-
-		if mpty == false {
+		if maxLocID := GetMaxLocID(DB); maxLocID == 0 {
 			if err := DestroyContainer(DB, existingUser.RootLoc, existingUser.Username); err != nil {
 				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 				tx.Rollback()
