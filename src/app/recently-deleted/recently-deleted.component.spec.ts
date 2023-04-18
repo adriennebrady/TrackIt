f7e07973531b5164a39eeef296309dda61bd379d
Sara <sara.winner@ufl.edu>
2023-04-18 11:45:27 -0400

    Fixed RecentlyDeletedComponent - should create

--- src/app/recently-deleted/recently-deleted.component.spec.ts
+++ src/app/recently-deleted/recently-deleted.component.spec.ts
@@ -1,8 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecentlyDeletedComponent } from './recently-deleted.component';
+import { SidebarNavComponent } from '../sidebar-nav/sidebar-nav.component';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatDialog } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTreeModule } from '@angular/material/tree';
 
 describe('RecentlyDeletedComponent', () => {
   let component: RecentlyDeletedComponent;
@@ -10,8 +21,22 @@ describe('RecentlyDeletedComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecentlyDeletedComponent ],
+      declarations: [ RecentlyDeletedComponent, SidebarNavComponent ],
+      imports: [
+        HttpClientTestingModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatTreeModule,
+        RouterTestingModule
+      ],
       providers: [
+        HttpClient,
+        HttpClientModule,
+        SidebarNavComponent,
         { provide: MatDialog, useValue: {} }
       ]
     })
