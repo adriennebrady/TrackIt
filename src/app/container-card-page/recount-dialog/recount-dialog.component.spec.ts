44c3a26decbf15177f2708628c45f4a3d9dae287
Sara <sara.winner@ufl.edu>
2023-04-18 17:51:17 -0400

    Fixed RecountDialogComponent - should create test

--- src/app/container-card-page/recount-dialog/recount-dialog.component.spec.ts
+++ src/app/container-card-page/recount-dialog/recount-dialog.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecountDialogComponent } from './recount-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('RecountDialogComponent', () => {
   let component: RecountDialogComponent;
@@ -8,7 +14,18 @@ describe('RecountDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecountDialogComponent ]
+      declarations: [ RecountDialogComponent ],
+      imports: [
+        MatDialogModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
 
