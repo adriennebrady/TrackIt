33ab8c66f18f6307d73a9eb0e14995b2f3ff06b1
Sara <sara.winner@ufl.edu>
2023-04-18 18:37:54 -0400

    Fixed DeleteAccountDialogComponent - should create

--- src/app/settings/delete-account-dialog/delete-account-dialog.component.spec.ts
+++ src/app/settings/delete-account-dialog/delete-account-dialog.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteAccountDialogComponent } from './delete-account-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('DeleteAccountDialogComponent', () => {
   let component: DeleteAccountDialogComponent;
@@ -8,7 +14,18 @@ describe('DeleteAccountDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteAccountDialogComponent ]
+      declarations: [ DeleteAccountDialogComponent ],
+      imports: [
+        MatDialogModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
 
