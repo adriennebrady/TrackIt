e4776643a51d1a4484fb8a64cf2c27c6c3a461e7
Sara <sara.winner@ufl.edu>
2023-04-18 22:39:09 -0400

    New DeleteAccountDialogComponent unit tests

--- src/app/settings/delete-account-dialog/delete-account-dialog.component.spec.ts
+++ src/app/settings/delete-account-dialog/delete-account-dialog.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteAccountDialogComponent } from './delete-account-dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
@@ -36,5 +37,51 @@ describe('DeleteAccountDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display add item dialog title', () => {
+    expect(fixture.nativeElement.querySelector('h1').textContent).toContain('Delete Account?');
+  });
+
+  it('should call cancel() when cancel button is clicked', () => {
+    fixture.detectChanges();
+    spyOn(component, 'cancel');
+    const cancelButton = fixture.debugElement.query(By.css('.cancelButton')).nativeElement;
+    cancelButton.click();
+    expect(component.cancel).toHaveBeenCalled();
+  });
+
+  it('should disable delete button element when input fields are empty', () => {
+    const deleteButton = fixture.debugElement.query(By.css('.renameButton')).nativeElement;
+    const passwordInput = fixture.debugElement.query(By.css('input[name=password]')).nativeElement;
+    const confirmPasswordInput = fixture.debugElement.query(By.css('input[name=confirmpass]')).nativeElement;
+
+    passwordInput.value = '';
+    passwordInput.dispatchEvent(new Event('input'));
+    confirmPasswordInput.value = '';
+    confirmPasswordInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(deleteButton.disabled).toBeTruthy();
+  });
+
+  it('should call deleteAccount() when delete button is clicked', () => {
+    spyOn(component, 'deleteAccount');
+    const deleteButton = fixture.debugElement.query(By.css('.renameButton')).nativeElement;
+    deleteButton.click();
+    expect(component.deleteAccount).toHaveBeenCalled();
+  });
+
+  it('should enable delete when input fields are filled', () => {
+    const passwordInput = fixture.debugElement.query(By.css('input[name="password"]')).nativeElement;
+    const confirmPasswordInput = fixture.debugElement.query(By.css('input[name="confirmpass"]')).nativeElement;
+    const renameButton = fixture.debugElement.query(By.css('.renameButton')).nativeElement;
+    
+    passwordInput.value = 'password';
+    passwordInput.dispatchEvent(new Event('input'));
+    confirmPasswordInput.value = 'password';    
+    confirmPasswordInput.dispatchEvent(new Event('input'));
+    
+    fixture.detectChanges();
+    expect(renameButton.disabled).toBeFalsy();
   });
 });
