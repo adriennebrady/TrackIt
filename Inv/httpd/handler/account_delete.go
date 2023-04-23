a67d4d99df9e5c35d22a9f98a646c57d9233d678
Israel Solano <israel.herrera@ufl.edu>
2023-04-23 13:12:49 -0400

    Fix account deletion recentlydeleted

--- Inv/httpd/handler/account_delete.go
+++ Inv/httpd/handler/account_delete.go
@@ -58,6 +58,13 @@ func AccountDelete(DB *gorm.DB) gin.HandlerFunc {
 			return
 		}
 
+		// Delete all containers
+		if result := tx.Table("recently_deleted_items").Where("account_id = ?", existingUser.Username).Delete(&RecentlyDeletedItem{}); result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+			tx.Rollback()
+			return
+		}
+
 		// Delete the account.
 		if result := tx.Table("accounts").Delete(&existingUser); result.Error != nil {
 			c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"error": "Couldn't delete account"})
